28d6c069f7ae092d1c875e91b9852d711547160c
nattapong pattanakit <yuminaxmvt2@gmail.com>
2026-04-25 13:07:54 +0700

    build: เปลี่ยนฐานโปรเจกต์เป็น Vue 3

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="soe">
+    <div class="soe__screen soe__screen--title">
+      <div class="soe__title-main">SHARDS OF</div>
+      <div class="soe__title-sub">ETERNITY</div>
+      <div class="soe__title-thai">กำลังย้ายเกมไป Vue 3</div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import "../shards_of_eternity.css";
+</script>
